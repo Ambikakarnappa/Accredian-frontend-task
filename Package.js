c0dc0ae83835132d0f06fa7084422c42d1ebb5dc
Ambikakarnappa <168699140+Ambikakarnappa@users.noreply.github.com>
2024-07-11 23:18:33 +0530

    Add files via upload

---
+++ Package.js
@@ -0,0 +1,27 @@
+{
+  "name": "refer-and-earn",
+  "version": "1.0.0",
+  "description": "A React application for referring and earning rewards",
+  "main": "index.js",
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+  "dependencies": {
+    "@mui/material": "^5.0.0",
+    "@emotion/react": "^11.0.0",
+    "@emotion/styled": "^11.0.0",
+    "react": "^18.0.0",
+    "react-dom": "^18.0.0",
+    "react-hook-form": "^7.0.0",
+    "react-scripts": "5.0.0"
+  },
+  "devDependencies": {},
+  "engines": {
+    "node": ">=14.0.0",
+    "npm": ">=6.0.0"
+  },
+  "license": "MIT"
+}
